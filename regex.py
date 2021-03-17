f884b6e38d392339ce3ff7ba5f9e62f9c79374c3
Ottmar Vargas <ottmar700@hotmail.com>
2021-03-17 07:44:56 -0600

    Se agregan mas ejemplos del curso de udemy al regex.py

--- regex.py
+++ regex.py
@@ -1,3 +1,21 @@
+'''
+The ? says the group matches zero or one times.
+The * says the group matches zero or more times.
+The + says the group matches one or more times.
+The curly braces can match a specific number of times.
+The curly braces with two numbers matches a minimum and maximum number of times.
+Leaving out the first or second number in the curly braces says there is no minimum or maximum.
+"Greedy matching" matches the longest string possible, "nongreedy matching" (or "lazy matching") matches the shortest string possible.
+Putting a question mark after the curly braces makes it do a nongreedy/lazy match.
+
+Groups are created in regex strings with parentheses.
+The first set of parentheses is group 1, the second is 2, and so on.
+Calling group() or group(0) returns the full matching string, group(1) returns group 1's matching string, and so on.
+Use \( and \) to match literal parentheses in the regex string.
+The | pipe can match one of many possible groups.
+'''
+
+
 # Search for lines that contain 'From'
 import re
 hand = open('mbox-short.txt')
@@ -100,3 +118,12 @@ for line in hand:
     print(x)
     count += 1
 print(count)
+
+
+phoneNumRegex = re.compile(r"(\d\d\d)-(\d\d\d-\d\d\d\d)")
+mo = phoneNumRegex.search("My number is 415-555-4242")
+print(phoneNumRegex)
+print(type(phoneNumRegex))
+print(mo.group())
+print(mo.group(1))
+print(mo.group(2))
