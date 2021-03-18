e4dd6ce49a9bb532b85849dabe688f4f54bc9a52
Ottmar Vargas <ottmar700@hotmail.com>
2021-03-18 07:38:15 -0600

    se  agrega mas informacion del curso de udemy al inicio del file como comentarios

--- regex.py
+++ regex.py
@@ -28,6 +28,11 @@ Pass re.DOTALL as the second argument to re.compile() to make the . dot match ne
 Pass re.I as the second argument to re.compile() to make the matching case-insensitive.
 
 
+The sub() regex method will substitute matches with some other text.
+Using \1, \2 and so will substitute group 1, 2, etc in the regex pattern.
+Passing re.VERBOSE lets you add whitespace and comments to the regex string passed to re.compile().
+If you want to pass multiple arguments (re.DOTALL , re.IGNORECASE, re.VERBOSE), combine them with the | bitwise operator.
+
 '''
 
 
